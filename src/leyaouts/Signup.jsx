a22474f592bc847df0616f69438cebeed76e4550
Jibon <manvirjibon@gmail.com>
2024-02-01 13:02:49 +0600

    navigate & validation name pass email

--- src/leyaouts/Signup.jsx
+++ src/leyaouts/Signup.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
-
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function Signup() {
+    const navigate =useNavigate();
     const [email,setemail] =useState('');
     const [name,setname] =useState('');
     const [password,setpassword] =useState('');
@@ -13,8 +16,24 @@ function Signup() {
      const handlePass = (e)=>{
         setpassword(e.target.value);
     }
+
+    const handleSubmit = ()=>{
+ if(!email){
+    toast.error("Email is required")
+ }
+  if(!name){
+    toast.error("Name is required")
+ }
+  if(!password){
+    toast.error("Password is required")
+ }
+ if(email && password && name){
+    navigate("/login")
+ }
+    }
   return (
     <div className=" mx-10 max-w-screen-lg">
+          <ToastContainer />
       <h2>Registration</h2>
       <div className=" flex flex-col gap-y-5">
         <input onChange={handleEmail} className="border py-2 px-4" type="text" placeholder="email" /> 
@@ -22,6 +41,7 @@ function Signup() {
         <input onChange={handleName} className="border py-2 px-4" type="text" placeholder="name" />
 
         <input   onChange={handlePass}  className="border py-2 px-4" type="text" placeholder="password" />
+        <button onClick={handleSubmit} type="text">Submit</button>
       </div>
     </div>
   )
